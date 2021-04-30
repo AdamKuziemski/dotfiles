96a0a61f7c3b5119ed2f2b1a64b14b327af9cb50
Adam Kuziemski <kuziemski.a@gmail.com>
2021-04-30 10:53:49 +0200

    changed updater to use relative path

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cp ~/.bashrc ~/Documents/projects/bash/dotfiles/.bashrc
-cp ~/.vimrc ~/Documents/projects/bash/dotfiles/.vimrc
-cp ~/.config/kglobalshortcutsrc ~/Documents/projects/bash/dotfiles/kglobalshortcutsrc
-cp ~/.config/khotkeysrc ~/Documents/projects/bash/dotfiles/khotkeysrc
-cp ~/.config/kwinrc ~/Documents/projects/bash/dotfiles/kwinrc
-cp ~/.config/kwinrulesrc ~/Documents/projects/bash/dotfiles/kwinrulesrc
+cp ~/.bashrc .
+cp ~/.vimrc .
+cp ~/.config/kglobalshortcutsrc .
+cp ~/.config/khotkeysrc .
+cp ~/.config/kwinrc .
+cp ~/.config/kwinrulesrc .
+cp ~/.config/kscreenlockerrc .
